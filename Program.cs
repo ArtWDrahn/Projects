bfffa7661fb6c0248c8d560bebe7a9e82cd9228f
Tyson J. Hayes <artwdrahn@gmail.com>
2013-07-29 23:35:55 -0700

    Reverse A String Complete
    Also wrote unit testing for program.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Tyson J. Hayes">
+//     Copyright (c), Tyson J. Hayes. All rights reserved.
+// </copyright>
+// <summary>
+//    Base Program
+// </summary>
+// <author>
+//    Tyson J. Hayes (jobs@tysonjhayes.com)
+// </author>
+//-----------------------------------------------------------------------
+
+namespace MegaProjectList
+{
+    /// <summary>
+    /// The program.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        public static void Main(string[] args)
+        {
+            // string input = "this is a string";
+        }
+    }
+}
---
+++ StringUnitTests/StringTests.cs
@@ -0,0 +1,36 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="StringTests.cs" company="Tyson J. Hayes">
+//     Copyright (c), Tyson J. Hayes. All rights reserved.
+// </copyright>
+// <summary>
+//    Testing for all of the string methods.
+// </summary>
+// <author>
+//    Tyson J. Hayes (jobs@tysonjhayes.com)
+// </author>
+//-----------------------------------------------------------------------
+
+namespace StringUnitTests
+{
+    using CustomString;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// The string tests.
+    /// </summary>
+    [TestClass]
+    public class StringTests
+    {
+        /// <summary>
+        /// The reverse a string unit test.
+        /// </summary>
+        [TestMethod]
+        public void ReverseAStringUnitTest()
+        {
+            const string TestString = "This is a test.";
+            const string ExpectedOutput = ".tset a si sihT";
+            Assert.AreEqual(Manipulate.Reverse(TestString), ExpectedOutput);
+        }
+    }
+}
